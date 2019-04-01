4fdb7608d301de0d5ccf474a02f0535ee4a83d76
Eshika <eshikagupta159@gmail.com>
2019-04-01 23:55:06 +0530

    Add files via upload
    Commit at 23:54

--- controllers/machineController.js
+++ controllers/machineController.js
@@ -4,9 +4,10 @@ const Sequelize = require('sequelize');
 var machineModel=require('../models/machineModel');
 const sequelize = new Sequelize('postgres://postgres:root@localhost:5432/postgres');
 
-
+//ADD MACHINE CODE
 exports.index=function(req,res){
-const machine= machineModel(sequelize,Sequelize);
+
+    const machine= machineModel(sequelize,Sequelize);
 
 machine.create(
     {
@@ -29,4 +30,53 @@ sequelize.sync({ force: false })
   });
 
   res.render('index',{title:"Machine added  "});
+}
+
+//VIEW MACHINE CODE
+
+exports.viewMachine=function (req,res) {
+    const machine= machineModel(sequelize,Sequelize);
+    machine.findAll({}).then((data) => {
+        //it will be objects of machine
+        console.log("View machines------------->"+data);
+    }).catch((err) => {
+        console.log(err);
+    });
+}
+
+//UPDATE MACHINE CODE
+
+exports.updateMachine=function (req,res) {
+    const machine= machineModel(sequelize,Sequelize);
+    machine.update({
+       // machineID:req.body.machineID,
+        machineName:req.body.machineName,
+        serialNo:req.body.serialNo,
+        modelID:req.body.modelID,
+        ip:req.body.ip,
+        machineStatus:req.body.machineStatus
+    }, {
+        where: {
+            id: req.body.machineID
+        }
+    }).then(() => {
+        console.log('Updated');
+    }).catch((e) => {
+        console.log("Error"+e);
+    });
+}
+
+
+//DELETE MACHINE CODE
+exports.deleteMachine=function() {
+    const machine= machineModel(sequelize,Sequelize);
+    machine.destroy({
+        where: {
+            id: 1
+        }
+    }).then(() => {
+        console.log("Deleted");
+    }).catch((e) => {
+        console.log("Error" + e);
+    });
 }
